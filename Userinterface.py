feccf0b2581acf745e5e229c04bbad391f491412
Dexter <gusevsergey85@gmail.comm>
2023-03-01 11:13:43 +0300

    Протекстировал программу. Все вроде работает.

--- Userinterface.py
+++ Userinterface.py
@@ -4,6 +4,7 @@ from Reading import read_all_notes
 from Editing import editing_note
 from Delete import del_note
 from Download import download_notes
+from Sorting import sort_note
 
 def user_interface():
     info = "Данная программа позволяет создавать, редактировать и просматривать заметки.\n" \
@@ -14,7 +15,9 @@ def user_interface():
            "edit - изменение заметок;\n" \
            "end - выход из программы;\n" \
            "delete - удалить заметку;\n" \
-           "download - выгрузить в файл JSON\n"
+           "download - выгрузить в файл JSON\n" \
+           "sort_ascend - сортировка по дате создания заметки по возрастанию\n" \
+           "sort_descend - сортировка по дате создания заметки по убыванию\n"
     while True:
         print("Введите команду: ")
         user_input = input().lower()
@@ -37,5 +40,11 @@ def user_interface():
             break
         elif user_input == "download":
             download_notes("note.json")
+        elif user_input == "sort_ascend":
+            sort_note("note.json", order=True)
+            print("Заметки отсортированы по возрастанию")
+        elif user_input == "sort_descend":
+            sort_note("note.json", order=False)
+            print("Заметки отсортированы по убыванию")
         else:
             print("Команда не найдена. help - справка о работе программы")
